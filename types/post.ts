473851fbe3ad2f690870a2a2e411c36352e5bfeb
Joanna Chądzyńska <jmalek87@gmail.com>
2021-06-26 21:16:13 +0200

    updated types

--- types/post.ts
+++ types/post.ts
@@ -1,145 +1,62 @@
-export interface Slug {
-    _key: string;
+export interface PostAuthorRef {
+    _ref: string;
     _type: string;
-    current: string;
 }
 
-export interface Geopoint {
+export interface PostBody {
     _key: string;
     _type: string;
-    lat: string;
-    lng: string;
-    alt: string;
-}
-
-export interface SanityImageDimensions {
-    _key: string;
-    _type: string;
-    height: string;
-    width: string;
-    aspectRatio: string;
-}
-
-export interface SanityImagePalette {}
-
-export interface SanityImageMetadata {
-    _key: string;
-    _type: string;
-    location: Geopoint;
-    dimensions: SanityImageDimensions;
-    palette: SanityImagePalette;
-    lqip: string;
-    hasAlpha: boolean;
-    isOpaque: boolean;
-}
-
-export interface SanityAssetSourceData {
-    _key: string;
-    _type: string;
-    name: string;
-    id: string;
-    url: string;
-}
-
-export interface Category {
-    _id: string;
-    _type: string;
-    _createdAt: string;
-    _updatedAt: string;
-    _rev: string;
-    _key: string;
-    title: string;
-    description: string;
-}
-
-export interface SanityImageAsset {
-    _id: string;
-    _type: string;
-    _createdAt: string;
-    _updatedAt: string;
-    _rev: string;
-    _key: string;
-    originalFilename: string;
-    label: string;
-    title: string;
-    description: string;
-    altText: string;
-    sha1hash: string;
-    extension: string;
-    mimeType: string;
-    size: string;
-    assetId: string;
-    path: string;
-    url: string;
-    metadata: SanityImageMetadata;
-    source: SanityAssetSourceData;
-}
-export interface SanityImageHotspot {
-    _key: string;
-    _type: string;
-    x: string;
-    y: string;
-    height: string;
-    width: string;
-}
-export interface SanityImageCrop {
-    _key: string;
-    _type: string;
-    top: string;
-    bottom: string;
-    left: string;
-    right: string;
-}
-
-export interface Image {
-    _key: string;
-    _type: string;
-    asset: SanityImageAsset;
-    hotspot: SanityImageHotspot;
-    crop: SanityImageCrop;
-}
-
-export interface Raw {
-    _key: string;
-    _type: string;
-    children: [
-        {
-            _key: string;
-            _type: string;
-            marks: [];
-            text: string;
-        }
-    ];
+    children: PostBodyChildren[];
     markDefs: [];
     style: string;
 }
 
-export interface Author {
-    _id: string;
-    _type: string;
-    _createdAt: string;
-    _updatedAt: string;
-    _rev: string;
+export interface PostBodyChildren {
     _key: string;
-    name: string;
-    slug: Slug;
-    image: Image;
-    bioRaw: Raw[];
+    _type: string;
+    marks: [];
+    text: string;
+}
+
+export interface PostMainImageRef {
+    _type: string;
+    alt: string;
+    asset: {
+        _ref: string;
+        _type: string;
+    };
+}
+
+export interface ImageGallery {
+    _key: string;
+    _type: string;
+    alt: string;
+    asset: {
+        _id: string;
+        url: string;
+    };
+}
+export interface PostSlug {
+    _type: string;
+    current: string;
+}
+
+export interface PostImage {
+    alt: string;
+    asset: {
+        _id: string;
+        url: string;
+    };
 }
 
 export interface Post {
-    _id: number;
-    _type: string;
-    _createdAt: string;
-    _updatedAt: string;
-    _rev: string;
-    _key: string;
-    title: string;
+    _id: string;
+    author: PostAuthorRef;
+    body: PostBody[];
     excerpt: string;
-    slug: Slug;
-    author: Author;
-    mainImage: Image;
-    categories: [Category];
+    mainImage: PostMainImageRef | PostImage;
+    imagesGallery: ImageGallery[];
     publishedAt: string;
-    bodyRaw: Raw[];
+    slug: PostSlug;
+    title: string;
 }
