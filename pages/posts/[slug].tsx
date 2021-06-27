e43ba448bd9f373ab5e1074a326f2200c69ae0ef
Joanna Chądzyńska <jmalek87@gmail.com>
2021-06-27 18:20:10 +0200

    update page

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,9 +1,11 @@
 import { Layout } from "components";
-// import {sanityClient, imageToString, usePreviewSubscription, PortableText} from 'lib/sanity'
-import { sanityClient } from "lib/sanity";
+// import {usePreviewSubscription} from 'lib/sanity'
+import { imageToString, PortableText, sanityClient } from "lib/sanity";
 import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { Post as IPost } from "types/post";
 
 const postQuery = `*[_type=="post" && slug.current == $slug][0]{
     "author": author->name,
@@ -27,13 +29,45 @@ const postQuery = `*[_type=="post" && slug.current == $slug][0]{
     }
 }`;
 
-const Post = (props: any) => {
-    console.log(props);
+const Post = ({ post }: { post: IPost }) => {
+    const { title, mainImage, body, publishedAt, author, imagesGallery } = post;
 
     return (
         <Layout>
-            <Head>{/* <title>{data.Post.title}</title> */}</Head>
+            <Head>
+                <title>{title.replace(/['"]+/g, "")}</title>
+            </Head>
             <article>
+                <h1>{title.replace(/['"]+/g, "")}</h1>
+                <main>
+                    <div
+                        style={{
+                            width: "100px",
+                            height: "200px",
+                            position: "relative",
+                        }}>
+                        <Image
+                            src={mainImage.asset?.url}
+                            alt={mainImage.alt}
+                            layout='fill'
+                        />
+                    </div>
+
+                    <PortableText blocks={body} />
+
+                    <br />
+                    <div>
+                        {imagesGallery.map((img) => (
+                            <img
+                                key={img._key}
+                                src={imageToString(img.asset.url)}
+                                alt={img.alt}
+                            />
+                        ))}
+                    </div>
+                    <small>{publishedAt}</small>
+                    <small>{author}</small>
+                </main>
                 {/* <h1>{data.Post.title.toString()}</h1>
                 <small>{data.Post.publishedAt}</small>
                 <p>{data.Post.bodyRaw.map((body) => body.children[0].text)}</p> */}
@@ -68,9 +102,7 @@ export const getStaticProps = async ({ params }: any) => {
     const post = await sanityClient.fetch(postQuery, { slug });
 
     return {
-        props: {
-            post,
-        },
+        props: { post },
     };
 };
 
