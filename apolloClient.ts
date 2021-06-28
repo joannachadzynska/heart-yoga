4a4b7eef72eaaf9335649bb3281a79a9007337b6
Joanna Chądzyńska <jmalek87@gmail.com>
2021-06-28 18:42:52 +0200

    remove unused files

--- apolloClient.ts
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const apolloClient = new ApolloClient({
-    uri: "https://8x435qri.api.sanity.io/v1/graphql/production/default",
-    cache: new InMemoryCache(),
-});
-
-export default apolloClient;
--- client.ts
+++
@@ -1,7 +0,0 @@
-import sanityClient from "@sanity/client";
-
-export default sanityClient({
-    projectId: "8x435qri",
-    dataset: "production",
-    useCdn: true,
-});
