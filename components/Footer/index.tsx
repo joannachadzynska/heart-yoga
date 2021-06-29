6b0193f72f9ea8b177358b106e181428f6286580
Joanna Chądzyńska <jmalek87@gmail.com>
2021-06-29 14:11:07 +0200

    fix footer update other styles install fontawesome packages setup fontawesome in main app

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,12 +1,59 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 import React from "react";
+import styles from "./footer.module.scss";
 
 export interface FooterProps {}
+
+export interface LinkProps {
+    href: string;
+    label?: string;
+}
+
+const SocialLink: React.FC<LinkProps> = ({ href, children, label }) => {
+    return (
+        <Link href={href}>
+            <a
+                className={styles.link}
+                target='_blank'
+                rel='noopener noreferrer'
+                role='link'
+                aria-label={label}>
+                {children}
+            </a>
+        </Link>
+    );
+};
 
 const Footer: React.SFC<FooterProps> = () => {
     return (
         <footer>
-            <div className='container'>
-                <small>&copy; Joanna Chądzyńska</small>
+            <div className={`container ${styles.footer}`} id='contact'>
+                <div className={styles.socials}>
+                    <SocialLink
+                        href='https://www.instagram.com/kingaariiyoga/'
+                        label='Kinga Arii Yoga instagram'>
+                        <FontAwesomeIcon icon={["fab", "instagram"]} />
+                    </SocialLink>
+                    <SocialLink
+                        href='https://www.facebook.com/kingaariiyoga'
+                        label='Kinga Arii Yoga facebook'>
+                        <FontAwesomeIcon icon={["fab", "facebook"]} />
+                    </SocialLink>
+                    <SocialLink
+                        href='mailto:kingaarii@wp.pl;jmalek87@gmail.com?subject=Zapytanie o kurs'
+                        label='Kinga Arii Yoga email'>
+                        <FontAwesomeIcon icon={["fas", "envelope"]} />
+                    </SocialLink>
+                    <SocialLink
+                        href='tel:+48731666097'
+                        label='Kinga Arii Yoga phone'>
+                        <FontAwesomeIcon icon={["fas", "phone-alt"]} />
+                    </SocialLink>
+                </div>
+                <div className={styles.copy}>
+                    <small>&copy; Joanna Chądzyńska</small>
+                </div>
             </div>
         </footer>
     );
