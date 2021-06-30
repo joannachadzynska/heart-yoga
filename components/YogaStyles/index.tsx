da7c8bbb292d04cc1c376ae0998369380b41904f
Joanna Chądzyńska <jmalek87@gmail.com>
2021-06-30 21:15:33 +0200

    update components

--- components/YogaStyles/index.tsx
+++ components/YogaStyles/index.tsx
@@ -1,11 +1,31 @@
+import { Course } from "@/types/course";
+import Link from "next/link";
 import * as React from "react";
 
-export interface YogaStyles {}
+export interface YogaStyles {
+    courses: Course[];
+}
 
-const YogaStyles: React.SFC<YogaStyles> = () => {
+const YogaStyles: React.SFC<YogaStyles> = ({ courses }) => {
+    console.log(courses);
+
     return (
-        <section className='container' id='about-me'>
-            <h1>YogaStyles section</h1>
+        <section className='courses-home__container' id='about-me'>
+            <div className='courses-home'>
+                {courses.map((course) => (
+                    <div className='courses-home__card' key={course._id}>
+                        <div
+                            className='courses-home__card-content'
+                            style={{
+                                backgroundImage: `url(${course.mainImage.asset.url})`,
+                            }}>
+                            <Link href={`/yoga-courses/${course.slug.current}`}>
+                                {course.title}
+                            </Link>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </section>
     );
 };
